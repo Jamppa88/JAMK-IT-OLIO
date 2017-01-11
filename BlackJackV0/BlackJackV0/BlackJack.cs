11fbafa0a4bc5f9e719886fa482c4c12ce3ed2bc
Jamppa88 <repse88@gmail.com>
2017-01-11 14:02:35 +0200

    blackjack ready

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -8,27 +8,56 @@ namespace JAMK.IT
   {
     static void Main()
     {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
-        {
-          System.Console.WriteLine("You win.");
-        }
-        else {
-          System.Console.WriteLine("You lose.");
-        }
-      }
+            // Initializing rng and local variables
+            Random rng = new Random();
+            int myNumber;
+            int theirNumber;
+            string input;
+            System.Console.WriteLine("*** BlackJack! ***");
+            while (true)
+            {
+                System.Console.Write("Can you beat my number? If you want to exit, type X or exit.\nEnter any number between 1-21: ");
+                // taking input first to a string
+                input = Console.ReadLine();
+                // exit commands
+                if (input == "X" || input == "exit" || input == "x")
+                {
+                    Console.WriteLine("Thanks for playing! :)");
+                    break;
+                }
+                // trying to see if input is valid
+                else if (int.TryParse(input,out theirNumber) == false)
+                {
+                    Console.WriteLine("Type a number or X to exit! Try again.");
+                }
+                // Actual game
+                else
+                {
+                    // converting
+                    theirNumber = System.Convert.ToInt32(input);
+                    myNumber = rng.Next(10, 21);
+                    //comparing that given number is valid
+
+                    if (theirNumber < 1 || theirNumber > 21)
+                    {
+                        Console.WriteLine("The given number is out of limits, try again.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("My number is " + myNumber);
+                        //comparing
+                        if (theirNumber >= myNumber && theirNumber <= 21)
+                        {
+                            System.Console.WriteLine("You win.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You lose.");
+                        }
+                    }
+                }
+            } // while
+        Console.ReadLine();
     }
   }
 }
